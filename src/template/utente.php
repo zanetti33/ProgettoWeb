752990983497d08019668d1c3231b5b576f1ff3f
zanetti33 <lorenzo.zanetti5@studio.unibo.it>
2022-02-07 21:39:49 +0100

    design of the user page

--- src/template/utente.php
+++ src/template/utente.php
@@ -1,6 +1,88 @@
 			<section>
                 <h2>I miei ordini</h2>
                 <div>
-                    
+                    <h2> 14 Dicembre 2020 </h2>
+                    <table>
+                        <tr>
+                            <th>Modello</th>
+                            <th>Colore</th>
+                            <th>Taglia</th>
+                            <th>Genere</th>
+                            <th>Scritta</th>
+                            <th>Numero</th>
+                            <th>Quantità</th>
+                            <th>Costo</th>
+                        </tr>
+                        <tr>
+                            <th>345345</th>
+                            <th>blu</th>
+                            <th>xl</th>
+                            <th>uomo</th>
+                            <th></th>
+                            <th></th>
+                            <th>3</th>
+                            <th>30$</th>
+                        </tr>
+                        <tr>
+                            <th>636</th>
+                            <th>giallo</th>
+                            <th>xs</th>
+                            <th>donna</th>
+                            <th>Giulia</th>
+                            <th>15</th>
+                            <th>1</th>
+                            <th>20$</th>
+                        </tr>
+                    </table>
+                    <p>Totale: 50$
+                        Stato: boh
+                    </p>
                 </div>
+                <div>
+                    <h2> 14 Dicembre 2020 </h2>
+                    <table>
+                        <tr>
+                            <th>Modello</th>
+                            <th>Colore</th>
+                            <th>Taglia</th>
+                            <th>Genere</th>
+                            <th>Scritta</th>
+                            <th>Numero</th>
+                            <th>Quantità</th>
+                            <th>Costo</th>
+                        </tr>
+                        <tr>
+                            <th>345345</th>
+                            <th>blu</th>
+                            <th>xl</th>
+                            <th>uomo</th>
+                            <th></th>
+                            <th></th>
+                            <th>3</th>
+                            <th>30$</th>
+                        </tr>
+                        <tr>
+                            <th>636</th>
+                            <th>giallo</th>
+                            <th>xs</th>
+                            <th>donna</th>
+                            <th>Giulia</th>
+                            <th>15</th>
+                            <th>1</th>
+                            <th>20$</th>
+                        </tr>
+                    </table>
+                    <p>Totale: 50$
+                        Stato: boh
+                    </p>
+                </div>
+            </section>
+            <section>
+                <h2>Credenziali</h2>
+                <p>Email: lorenzo.zanetti33@gmail.com</p>
+                <form action="#" method="POST">
+                    <label for="password">Nuova password:</label>
+                    <input type="password" id="password" name="password" />
+                    <input type="submit" name="submit" value="Invia" />
+                </form>
             </section>
