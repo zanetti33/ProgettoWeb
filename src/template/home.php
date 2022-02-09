034b4020e7014d284faa6c2c60ecfa276985101c
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2022-02-09 09:57:59 +0100

    home img navigable

--- src/template/home.php
+++ src/template/home.php
@@ -1,18 +1,24 @@
 			<section>
 				<h2><a href="prodotti.php?genere=U">Uomo</a></h2>
                 <div>
-                    <img <?php isActive("prodotti.php?genere=U");?> src="./img/ProdottiUomo.jpg" alt="prodotti uomo" />
+                    <a href="prodotti.php?genere=U">
+                        <img <?php isActive("prodotti.php?genere=U");?> src="./img/ProdottiUomo.jpg" alt="prodotti uomo" />
+                    </a>
                 </div>
             </section>
             <section>
                 <h2><a href="prodotti.php?genere=D">Donna</a></h2>
                 <div>
-                    <img <?php isActive("prodotti.php?genere=D");?> src="./img/ProdottiDonna.jpg" alt="prodotti donna"/>
+                    <a href="prodotti.php?genere=D">
+                        <img <?php isActive("prodotti.php?genere=D");?> src="./img/ProdottiDonna.jpg" alt="prodotti donna"/>
+                    </a>
                 </div>
             </section>
             <section>
                 <h2><a href="prodotti.php?genere=B">Bambino</a></h2>
                 <div>
-                    <img src="./img/ProdottiBambino.jpg" alt="prodotti bambino" />
+                    <a href="prodotti.php?genere=B">
+                        <img src="./img/ProdottiBambino.jpg" alt="prodotti bambino" />
+                    </a>
                 </div>
             </section>
